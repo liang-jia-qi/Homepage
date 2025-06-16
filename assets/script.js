2d6157be1525c18d92bf7288b45834018e2b7c15
liang-jia-qi <liang9804angel@gmail.com>
2025-06-16 16:53:06 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,16 @@
 // assets/script.js
-document.addEventListener('DOMContentLoaded', function () {
-  if (document.getElementById('vanta-bg')) {
-    VANTA.WAVES({
-      el: "#vanta-bg",
-      mouseControls: true,
-      touchControls: true,
-      minHeight: 200.00,
-      minWidth: 200.00,
-      scale: 1.0,
-      scaleMobile: 1.0,
-      color: 0x111111,
-      shininess: 50.0,
-      waveHeight: 15.0,
-      waveSpeed: 0.75,
-      zoom: 0.85
-    });
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  VANTA.GLOBE({
+    el: "#vanta-bg",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0xcaabca,
+    color2: 0xb69b7f,
+    backgroundColor: 0x000000
+  });
 });
